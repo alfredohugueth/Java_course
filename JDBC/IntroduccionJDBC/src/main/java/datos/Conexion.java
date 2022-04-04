18c4d7782e9664bd74151ccd258c6cecb7174f15
Alfredo Hugueth Polo <alfredohugueth99@gmail.com>
2022-04-03 21:36:58 -0500

    Adding datasource for pool connections and git ignore for target files generated

--- JDBC/IntroduccionJDBC/src/main/java/datos/Conexion.java
+++ JDBC/IntroduccionJDBC/src/main/java/datos/Conexion.java
@@ -1,5 +1,8 @@
 package datos;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
 import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 
@@ -7,6 +10,17 @@ public class Conexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "q6winaj3";
+
+    public static DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(JDBC_URL);
+        dataSource.setUsername(JDBC_USER);
+        dataSource.setPassword(JDBC_PASSWORD);
+
+        // Definimos el tamaño inicial del pool de conexiones
+        dataSource.setInitialSize(5);
+        return dataSource;
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
